24297a826b8470b9b45afa5ba4f4926b12a4d180
Adrien <adrien.biz@gmail.com>
2023-04-09 15:23:44 +0400

    Set up header and custom fonts

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="flex justify-center bg-black">
+      <div className="py-6 font-futurama-bold font-bold text-transparent text-5xl bg-clip-text bg-gradient-to-t from-purple-400 to-pink-600">
+        FUTURAMA MEMORY GAME
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'futurama-title': ['futurama-title-font', 'sans-serif'],
+        'futurama-bold': ['futurama-bold-font', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
