19faa1e362f1eb8101c02f2bd05150d8fe41fa52
Adrien <adrien.biz@gmail.com>
2023-04-10 20:07:32 +0400

    Randomize characters

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,27 +3,38 @@ import { useState } from 'react';
 function Cards() {
   const futuramaCharacters = [
     {
+      id: 1,
       name: 'Philip J. Fry',
       avatarUrl: 'https://i.imgur.com/lWAi2sT.png',
     },
     {
+      id: 2,
       name: 'Turanga Leela',
       avatarUrl: 'https://i.imgur.com/gTK0CAr.png',
     },
     {
+      id: 3,
       name: 'Bender Bending Rodriguez',
       avatarUrl: 'https://i.imgur.com/c5ebN1S.png',
     },
     {
+      id: 4,
       name: 'Professor Hubert J. Farnsworth',
       avatarUrl: 'https://i.imgur.com/Ujmyf7E.png',
     },
   ];
 
-  const [characters] = useState(futuramaCharacters);
+  const [characters, setCharacters] = useState(
+    shuffleArray(futuramaCharacters)
+  );
+
+  function handleClick() {
+    const randomChar = shuffleArray(futuramaCharacters);
+    setCharacters([...randomChar]);
+  }
 
   const listCharacters = characters.map((character) => (
-    <Card character={character} />
+    <Card key={character.id} character={character} handleClick={handleClick} />
   ));
 
   return (
@@ -33,12 +44,23 @@ function Cards() {
   );
 }
 
-function Card({ character }) {
+function Card({ character, handleClick }) {
   return (
-    <div className="flex h-60 w-60 justify-center rounded-xl bg-white shadow-xl">
+    <button
+      className="flex h-60 w-60 justify-center rounded-xl bg-white shadow-xl"
+      onClick={handleClick}
+    >
       <div>{character.name}</div>
-    </div>
+    </button>
   );
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
 export default Cards;
