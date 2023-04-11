4b5aaca406cc51bcc449337d980c2f71478f315a
Adrien <adrien.biz@gmail.com>
2023-04-11 21:35:36 +0400

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="grid min-h-screen grid-rows-[min-content_1fr]">
+    <div className="grid h-screen grid-rows-[min-content_1fr]">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
